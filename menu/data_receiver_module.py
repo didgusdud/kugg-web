4c85fc93b2861721226328171d04d10233797ef9
ApinCan <strain2011@gmail.com>
2020-09-20 16:50:40 +0900

    usersleage, usersinfo test complete

--- menu/data_receiver_module.py
+++ menu/data_receiver_module.py
@@ -5,6 +5,7 @@ Created on Thu Sep  3 21:49:15 2020
 @author: 6794c
 """
 import pandas as pd
+import time
 
 import usersLeagueReceiver
 import usersInfoReceiver
@@ -16,7 +17,7 @@ import db_controller
 # input your api key when you need
 class DataReceiver:
     def __init__(self):
-        self.API_KEY = None
+        self.API_KEY = "RGAPI-0160fcec-9c62-44a2-a82b-df0af3e97bf6"
         self.usersLeague_csv_name = "usersLeague.csv"
         self.usersInfo_csv_name = "usersInfo.csv"
         self.usersMatchlist_csv_name = "usersMatchlist.csv"
@@ -40,38 +41,48 @@ class DataReceiver:
         
         
     def run(self):
-        # for i in range(5):
-        #     """test complete"""
-        #     usersLeague_df = self.usersLeague_receiver.run()
-        #     self.database_controller.update_usersLeague(usersLeague_df)
+        for i in range(3):
+            """test complete"""
+            usersLeague_df = self.usersLeague_receiver.run()
+            self.database_controller.update_usersLeague(usersLeague_df)
+            print("usersleague complete")
 
-        #     """not test"""
-        #     summonerId_array = self.database_controller.from_usersLeague_for_usersInfo_summonerId(usersLeague_df)
-        #     usersInfo_df = self.usersInfo_receiver.run(summonerId_array)
-        #     self.database_controller.update_usersInfo(usersInfo_df)
+            summonerId_array = self.database_controller.from_usersLeague_for_usersInfo_summonerId(usersLeague_df)
+            print("residual summonerid array: ", len(summonerId_array))
+            usersInfo_df = self.usersInfo_receiver.run(summonerId_array)
+            
+            if len(usersInfo_df)!=0:
+                self.database_controller.update_usersInfo(usersInfo_df)
+            print("usersinfo complete")
+            
+            # usersMatchlist_df = self.usersMatchlist_receiver.run(self.database_controller.load_usersInfo_fromDB())
+            # self.db_controller.update_usersMatchlist(usersMatchlist_df)
+            # print("usersmatchlist complete")
+            
+            time.sleep(5)
+        self.database_controller.controller_close()
 
-        #     usersMatchlist_df = self.usersMatchlist_receiver.run(self.database_controller.load_usersInfo_fromDB())
-        #     self.db_controller.update_usersMatchlist(usersMatchlist_df)
             
-        #     unique_gameId = self.unique_matchList(usersMatchlist_df)
-        #     matchInfo_df = self.matchInfo_receiver.run(unique_gameId)
-        #     self.db_controller.update_matchInfo(matchInfo_df)
             
-        #     matchTimeline_df = self.matchTimeline_receiver.run(unique_gameId)
-        #     self.db_controller.update_matchTimeline(matchTimeline_df)
+            # unique_gameId = self.unique_matchList(usersMatchlist_df)
+            # matchInfo_df = self.matchInfo_receiver.run(unique_gameId)
+            # self.db_controller.update_matchInfo(matchInfo_df)
+            
+            # matchTimeline_df = self.matchTimeline_receiver.run(unique_gameId)
+            # self.db_controller.update_matchTimeline(matchTimeline_df)
 
         # default code
-        usersLeague_df = self.usersLeague_receiver.run()
-        print("usersLeague complete")   
-        usersInfo_df = self.usersInfo_receiver.run(usersLeague_df)
-        print("usersInfo complete")
-        usersMatchlist_df = self.usersMatchlist_receiver.run(usersInfo_df)
-        print("usersMatchlist complete")
-        unique_gameId = self.unique_matchList(usersMatchlist_df)
-        self.matchInfo_receiver.run(unique_gameId)
-        print("matchInfo complete")
-        self.matchTimeline_receiver.run(unique_gameId)
-        print("matchTimeline complete")
+        # usersLeague_df = self.usersLeague_receiver.run()
+        # print("usersLeague complete")   
+        # usersInfo_df = self.usersInfo_receiver.run(usersLeague_df)
+        # print("usersInfo complete")
+        # usersMatchlist_df = self.usersMatchlist_receiver.run(usersInfo_df)
+        # print("usersMatchlist complete")
+        # unique_gameId = self.unique_matchList(usersMatchlist_df)
+        # self.matchInfo_receiver.run(unique_gameId)
+        # print("matchInfo complete")
+        # self.matchTimeline_receiver.run(unique_gameId)
+        # print("matchTimeline complete")
 
         # usersMatchlist_df = pd.read_csv("usersMatchlist.csv")
         # columns_df = pd.read_csv("columnsInfo_web.csv")
--- menu/db_controller.py
+++ menu/db_controller.py
@@ -4,7 +4,7 @@ Created on Tue Sep  8 14:38:24 2020
 
 @author: 6794c
 """
-import sqlite3
+import pymysql
 import copy
 import numpy as np
 import pandas as pd
@@ -14,7 +14,7 @@ class DBController:
         self.conn, self.cur = self.db_connect()       
         
     def db_connect(self):
-        conn = sqlite3.connect("../lolraw.db")
+        conn = pymysql.connect(host="localhost", user='loladmin', password='loladmin', db='lolanlysis')
         cur = conn.cursor()
         
         return conn, cur
@@ -23,17 +23,15 @@ class DBController:
     def update_usersLeague(self, usersLeague_df):
         # tier, leagueId, queue, name, summonerId, summonerName, leaguePoints,
         #       rank, wins, losses, veteran, inactive, freshBlood, hotStreak
-        insert_usersLeague_sql = "INSERT into usersLeague \
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        update_usersLeague_sql = "UPDATE usersLeague \
-            SET tier=?, leagueId=?, queue=?, name=?, summonerName=?,\
-                leaguePoints=?, rank=?, wins=?, losses=?, veteran=?, inactive=?,\
-                freshBlood=?, hotStreak=?\
-            WHERE summonerId=?"
-        delete_usersLeague_sql = "DELETE FROM usersLeague\
-            WHERE summonerId=?"
+        insert_usersLeague_sql = "INSERT into usersleague \
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        update_usersLeague_sql = "UPDATE usersleague \
+            SET tier=%s, leagueId=%s, queue=%s, lname=%s, summonerName=%s, leaguePoints=%s, srank=%s, wins=%s, losses=%s, veteran=%s, sinactive=%s, freshBlood=%s, hotStreak=%s \
+                WHERE summonerId=%s;"
+        delete_usersLeague_sql = "DELETE FROM usersleague\
+            WHERE summonerId=%s"
             
-        self.cur.execute("SELECT * FROM usersLeague")
+        self.cur.execute("SELECT * FROM usersleague")
         rows = self.cur.fetchall()
         request_usersLeauge_summonerId = usersLeague_df["summonerId"].values
         residual_summonerId = copy.deepcopy(request_usersLeauge_summonerId)
@@ -43,99 +41,113 @@ class DBController:
             if row[4] in request_usersLeauge_summonerId:
                 # DB'S leaguepoints not same request leaguePoints
                 residual_summonerId = np.delete(residual_summonerId, np.argwhere(residual_summonerId==row[4]))
+                # print("0", row[6], int(usersLeague_df[usersLeague_df["summonerId"]==row[4]]["leaguePoints"].values[0]))
                 if int(row[6]) != int(usersLeague_df[usersLeague_df["summonerId"]==row[4]]["leaguePoints"].values[0]):
-                    # print(row[5], row[6], usersLeague_df[usersLeague_df["summonerId"]==row[4]]["leaguePoints"].values[0])
-                    self.cur.execute(update_usersLeague_sql,
-                                usersLeague_df[usersLeague_df["summonerId"]==row[4]].drop("summonerId", axis=1).astype(str).values[0].tolist()+[row[4]])
+                    
+                    update_row = usersLeague_df[usersLeague_df["summonerId"]==row[4]].values[0].tolist()
+                    update_row.append(row[4])
+                    del update_row[4]
+
+                    self.cur.execute(update_usersLeague_sql, update_row)
             else:
                 # print(row[5], row[6])
                 self.cur.execute(delete_usersLeague_sql, (row[4],))
-                
+        
+        self.conn.commit()
+
+        print("usersleague residual summerlength: ", len(residual_summonerId))
+
         # insert data which not inside DB
         for summonerId in residual_summonerId:
             self.cur.execute(insert_usersLeague_sql,
-                             usersLeague_df[usersLeague_df["summonerId"]==summonerId].astype(str).values[0])
+                             tuple(usersLeague_df[usersLeague_df["summonerId"]==summonerId].values[0]))
             
         self.conn.commit()
         # self.conn.close()
         
+        return 0
+        
         # extract summonerId from usersLeague for request usersInfo
-        def from_usersLeague_for_usersInfo_summonerId(self, usersLeague_df):
-            delete_usersInfo_sql = "DELETE FROM usersInfo WHERE summonerId=?"
-            select_usersInfo_sql = "SELECT * FROM usersInfo"
-            self.cur.execute(select_usersInfo_sql)
+    def from_usersLeague_for_usersInfo_summonerId(self, usersLeague_df):
+        delete_usersInfo_sql = "DELETE FROM usersInfo WHERE summonerId=%s"
+        select_usersInfo_sql = "SELECT * FROM usersInfo"
+        self.cur.execute(select_usersInfo_sql)
+        
+        request_usersLeague_summonerId = usersLeague_df["summonerId"].values
+        residual_summonerId = copy.deepcopy(request_usersLeague_summonerId)
+             
+        for row in self.cur.fetchall():
+            residual_summonerId = np.delete(residual_summonerId, np.argwhere(residual_summonerId==row[0]))
+            if row[0] not in request_usersLeague_summonerId:
+            # is in DB, but not in latest usersLeague --> need delete
+                self.cur.execute(delete_usersInfo_sql, row[0])
             
-            request_usersLeague_summonerId = usersLeague_df["summonerId"].values
-            residual_summonerId = copy.deepcopy(request_usersLeague_summonerId)
-                 
-            for row in self.cur.fetchall():
-                residual_summonerId = np.delete(residual_summonerId, np.argwhere(residual_summonerId==row[0]))
-                if row[0] in request_usersLeague_summonerId:
-                    pass
-                # is in DB, but not in latest usersLeague --> need delete
-                else:
-                    self.cur.execute(delete_usersInfo_sql, (row[0], ))
-                
-            self.conn.commit()
-            
-            return residual_summonerId
+        self.conn.commit()
+        
+        return residual_summonerId
         
         
-        """
-        In usersInfo part, did not implement update sql because of too much requestt
-        """
-        def update_usersInfo(self, usersInfo_df):
-            # id, accountId, puuid, name, profileIconId, revesionDate, summonerLevel
-            insert_usersInfo_sql = "INSERT INTO usersInfo VALUES (?, ?, ?, ?, ?, None, ?)"
-                
-            for row in usersInfo_df.values:
-                self.cur.execute(insert_usersInfo_sql, row.astype(str).tolist())
-                
-            self.conn.commit()
+    """
+    In usersInfo part, did not implement update sql because of too much requestt
+    """
+    def update_usersInfo(self, usersInfo_df):
+        # id, accountId, puuid, name, profileIconId, revesionDate, summonerLevel
+        insert_usersInfo_sql = "INSERT INTO usersinfo (summonerId, accountId, puuId, sname, profileIconId, \
+            summonerLevel) VALUES (%s, %s, %s, %s, %s, %s)"
+
+        for row in usersInfo_df.values:
+            insert_row = row.tolist()
+            del insert_row[-2]
+            self.cur.execute(insert_usersInfo_sql, insert_row)
             
-        def load_usersInfo_fromDB(self):
-            select_usersInfo_sql = "SELECT accountId, revisionDate FROM usersInfo"
-            
-            usersInfo_df = pd.read_sql_query(select_usersInfo_sql, self.conn)
-            
-            return usersInfo_df
+        self.conn.commit()
         
-        # only given new matchlist.
-        # insert new matchlist data to matchlistDB and update and update usersInfo revisionDate column
-        def update_usersMatchlist(self, usersMatchlist_df):
-            # index, platformId, gameId, champion, queue, season, (begin)timestamp, role, lane, accountId
-            insert_usersMatchlist_sql = "INSERT INTO usersInfo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-            update_usersInfo_sql = "UPDATE usersInfo SET revisionDate=? WHERE accountId=?"
-            
-            before_account = "iniail_account"  # row[-1]
-            recent_timestamp = 100 # row[5]
-            for row in usersMatchlist_df.values:
-                self.cur.execute(insert_usersMatchlist_sql, row.astype(str).tolist())
-                if row[-1] != before_account:
-                    before_account = row[-1]
-                    recent_timestamp = row[5]
-                    
-                    # update usersInfo revisionDate 
-                    self.cur.execute(update_usersInfo_sql, [recent_timestamp, before_account])
+    def load_usersInfo_fromDB(self):
+        select_usersInfo_sql = "SELECT accountId, revisionDate FROM usersInfo"
+        
+        usersInfo_df = pd.read_sql_query(select_usersInfo_sql, self.conn)
+        
+        
+        return usersInfo_df
+        
+    # only given new matchlist.
+    # insert new matchlist data to matchlistDB and update and update usersInfo revisionDate column
+    def update_usersMatchlist(self, usersMatchlist_df):
+        # index, platformId, gameId, champion, queue, season, (begin)timestamp, role, lane, accountId
+        insert_usersMatchlist_sql = "INSERT INTO usersInfo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        update_usersInfo_sql = "UPDATE usersInfo SET revisionDate=? WHERE accountId=?"
+        
+        before_account = "iniail_account"  # row[-1]
+        recent_timestamp = 100 # row[5]
+        for row in usersMatchlist_df.values:
+            self.cur.execute(insert_usersMatchlist_sql, row.astype(str).tolist())
+            if row[-1] != before_account:
+                before_account = row[-1]
+                recent_timestamp = row[5]
                 
-            self.conn.commit()
+                # update usersInfo revisionDate 
+                self.cur.execute(update_usersInfo_sql, [recent_timestamp, before_account])
             
-        """matchInfo, matchTimeline method only excute insert function"""
-        def update_matchInfo(self, matchInfo_df):
-            insert_matchInfo_sql = "INSERT INTO matchInfo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        self.conn.commit()
+        
+    """matchInfo, matchTimeline method only excute insert function"""
+    def update_matchInfo(self, matchInfo_df):
+        insert_matchInfo_sql = "INSERT INTO matchInfo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        
+        for row in matchInfo_df.values:
+            try:
+                self.cur.execute(insert_matchInfo_sql, row)
+            except IntegrityError:
+                pass
             
-            for row in matchInfo_df.values:
-                try:
-                    self.cur.execute(insert_matchInfo_sql, row)
-                except IntegrityError:
-                    pass
-                
-        def update_matchTimeline(self, matchTimeline_df):
-            insert_matchTimeline_sql = "INSERT INTO matchTimeline VALUES (?, ?, ?)"
-            
-            for row in matchTimeline_df:
-                try:
-                    self.cur.execute(insert_matchTimeline_sql, row)
-                except IntegrityError:
-                    pass
-            
+    def update_matchTimeline(self, matchTimeline_df):
+        insert_matchTimeline_sql = "INSERT INTO matchTimeline VALUES (?, ?, ?)"
+        
+        for row in matchTimeline_df:
+            try:
+                self.cur.execute(insert_matchTimeline_sql, row)
+            except IntegrityError:
+                pass
+    
+    def controller_close(self):
+        self.conn.close()
